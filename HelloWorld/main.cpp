852498ec5ab6701d1c79feabcfeacd77b0a2824e
Jacob Neal <jacob.neal@oit.edu>
2014-11-03 19:05:03 -0800

    Added Hello World Project
    This is just a test project to get a Qt project added to our repository
    to get the feel for Git.

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QGridLayout>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QWidget win;
+
+    QLabel hello("Hello World");
+
+    QGridLayout layout;
+
+    layout.addWidget(&hello);
+    win.setLayout(&layout);
+
+    win.show();
+
+    return app.exec();
+}
