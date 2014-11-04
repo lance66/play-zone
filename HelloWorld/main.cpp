0f24a8ee4b2fb87aa7cf42f0b79dcd8b91bfb353
LewisSanchez <lewis.sanchez@oit.edu>
2014-11-03 19:47:36 -0800

    Made modification to helloworld
    Modified the string with an exclamation mark.

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QWidget win;
 
-    QLabel hello("Hello World");
+    QLabel hello("Hello World!");
 
     QGridLayout layout;
 
