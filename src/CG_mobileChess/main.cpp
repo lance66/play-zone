bdab7a7b68eb2bda40f394f990bfbdb81a138a8a
StewartTaylor <stewart.taylor@oit.edu>
2014-11-25 11:44:43 -0800

    Added stylesheet to widget
    Changed widget to baby blue background.

--- src/CG_mobileChess/main.cpp
+++ src/CG_mobileChess/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QWidget win;
+    win.setStyleSheet("background-color: #448ed3");
 
     CG_login login(&win);
     QGridLayout win_layout;
