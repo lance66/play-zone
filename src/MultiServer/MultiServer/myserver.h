87fc7babf4005be3d4de458b1969a4334c13fabc
LewisSanchez <lewis.sanchez@oit.edu>
2015-01-18 01:22:37 -0800

    Created helper methods
    Created helper methods for the MyServer class to improve the readability
    of our code, make the code easier to debug,, and reduce code
    duplication, All added code has been tested, and documented. Pay no
    attention to the revert before this code commit. I accidentally clicked
    the revert button, when I was going to sync.

--- src/MultiServer/MultiServer/myserver.h
+++ src/MultiServer/MultiServer/myserver.h
@@ -28,30 +28,52 @@
 *       Accepts any pending connections.
 *   void sendMove(QTcpSocket *client)
 *       Sends the move to the client
+*   void addPlayerConnection(QTcpSocket *chessPlayer)
+*       Adds a player connection to the list of connections.
+*   void startOneMinuteMatch()
+*       Starts a one minute match, when there are two or more players
+*   void removeAllClientConnections(QTcpSocket *client)
+*       Removes all clients from the connection's list.
+*   void configureThreadSignalsAndSlots(MyThread * thread)
+*       Configures the signals and slots for a thread.
+*   void writeSocketDescriptorToSocket(QTcpSocket *client, qintptr socketDescriptor)
+*       Follows a series of conversions to write socketDescriptor
+*       to the socket.
+*   void delaySocket(QTcpSocket * socket, int timeToDelay)
+*       Waits for bytes to write to client.
 *************************************************************************/
 class MyServer : public QTcpServer
 {
     Q_OBJECT
-public:
-    explicit MyServer(QObject *parent = 0);
-    void StartServer();
+
+    public:
+        explicit MyServer(QObject *parent = 0);
+        void StartServer();
+
+    signals:
+
+    public slots:
 
 
+    protected slots:
+        void clientDisconnected();
 
-signals:
+    protected:
+        void incomingConnection(qintptr socketDescriptor);
+        void sendMove(QTcpSocket *client, qintptr socketDescriptor);
 
-public slots:
-protected slots:
-    void clientDisconnected();
+        QList<QTcpSocket *> clientConnections;
+        QVector<CG_Match> matches;
+        CG_Match match;
+        QQueue<int> oneMinuteQueue;
 
-protected:
-    void incomingConnection(qintptr socketDescriptor);
-    void sendMove(QTcpSocket *client, qintptr socketDescriptor);
-
-    QList<QTcpSocket *> clientConnections;
-    QVector<CG_Match> matches;
-    CG_Match match;
-    QQueue<int> oneMinuteQueue;
+    private:
+        void addPlayerConnection(QTcpSocket *chessPlayer);
+        void startOneMinuteMatch();
+        void removeAllClientConnections(QTcpSocket *client);
+        void configureThreadSignalsAndSlots(MyThread * thread);
+        void writeSocketDescriptorToSocket(QTcpSocket *client, qintptr socketDescriptor);
+        void delaySocket(QTcpSocket * socket, int timeToDelay);
 };
 
 #endif // MYSERVER_H
