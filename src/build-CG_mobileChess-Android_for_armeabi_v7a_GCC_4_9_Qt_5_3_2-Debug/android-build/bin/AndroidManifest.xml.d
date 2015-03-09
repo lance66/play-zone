daf264e6bec3ee9284d76eb9cae28e3f9517038c
StewartTaylor <stewart.taylor@oit.edu>
2015-03-08 21:41:45 -0700

    Reverted broken build
    Broken build = bad.  Fixed.

--- src/build-CG_mobileChess-Android_for_armeabi_v7a_GCC_4_9_Qt_5_3_2-Debug/android-build/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\admin\Documents\GitHub\play-zone\src\build-CG_mobileChess-Android_for_armeabi_v7a_GCC_4_9_Qt_5_3_2-Debug\android-build\bin\AndroidManifest.xml : \
-C:\Users\admin\Documents\GitHub\play-zone\src\build-CG_mobileChess-Android_for_armeabi_v7a_GCC_4_9_Qt_5_3_2-Debug\android-build\AndroidManifest.xml \
--- src/build-CG_mobileChess-Android_for_armeabi_v7a_GCC_4_9_Qt_5_3_2-Debug/android-build/gen/org/kde/necessitas/ministro/IMinistro.java.d
+++
@@ -1,5 +0,0 @@
-C:\Users\admin\Documents\GitHub\play-zone\src\build-CG_mobileChess-Android_for_armeabi_v7a_GCC_4_9_Qt_5_3_2-Debug\android-build\gen\org\kde\necessitas\ministro\IMinistro.java: \
-  C:\Users\admin\Documents\GitHub\play-zone\src\build-CG_mobileChess-Android_for_armeabi_v7a_GCC_4_9_Qt_5_3_2-Debug\android-build\src\org\kde\necessitas\ministro\IMinistro.aidl \
-  C:\Users\admin\Documents\GitHub\play-zone\src\build-CG_mobileChess-Android_for_armeabi_v7a_GCC_4_9_Qt_5_3_2-Debug\android-build\src\org\kde\necessitas\ministro\IMinistroCallback.aidl 
-
-C:\Users\admin\Documents\GitHub\play-zone\src\build-CG_mobileChess-Android_for_armeabi_v7a_GCC_4_9_Qt_5_3_2-Debug\android-build\src\org\kde\necessitas\ministro\IMinistroCallback.aidl :
